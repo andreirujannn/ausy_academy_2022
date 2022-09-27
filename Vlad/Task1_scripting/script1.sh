5259600ca70a109d95bf0599efd0b6962fa17400
VladVestem <vlad.vestemeanu@ulbsibiu.ro>
2022-09-27 21:26:39 +0300

    Task1_linux_scripting

---
+++ Vlad/Task1_scripting/script1.sh
@@ -0,0 +1,29 @@
+#! /bin/bash
+
+echo "Starting script..."
+# 1-3
+cd ~/Desktop/ausy_academy_2022/sandbox/vvestemeanu
+# 4-5
+git switch -c "Vlad_Script1"
+# 6
+touch "$MY_NAME.txt"
+# 7
+for i in {1..10}
+do
+    touch "gitMessage$i"
+    # 8
+    if [ $i -eq 5 ]; then
+        echo "$MY_NAME: my first script" >> gitMessage5
+    fi
+done
+# 9
+git add "$MY_NAME.txt"
+# 10
+commitmessage="$(grep "$MY_NAME: my first script" *)"
+# 11
+git commit -m "$commitmessage"
+# 12
+for i in {1..10}
+do
+    rm "gitMessage$i"
+done
