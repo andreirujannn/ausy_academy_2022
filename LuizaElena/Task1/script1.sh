00af66dfa5bae5ea08f79416611f299f71a36695
LuizaIvan <luizaivan8@gmail.com>
2022-09-28 13:16:18 +0300

    modificari

---
+++ LuizaElena/Task1/script1.sh
@@ -0,0 +1,26 @@
+#! /bin/bash
+
+cd ~/Desktop/ausy_academy_2022/sandbox/luiza
+git switch -c "Luiza_script"
+touch "$MY_NAME.txt"
+
+for i in {0..10}
+  do 
+     if [$i -eq 5 ];then
+        touch gitMessage5
+        echo "gitMessage5"
+        git commit -m "luiza_ivan:my first script"
+        else
+        touch gitMessage$i
+    fi
+
+    done
+
+    git add "$MY_NAME.txt"
+
+    git commit -m "first script"
+
+    for i in {0..10}
+    do 
+      rm "gitMessage$i"
+      done
