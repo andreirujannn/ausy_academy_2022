41879b51cd6c4479eb5d82c44d9bba3b99fe3bad
VladVestem <vlad.vestemeanu@ulbsibiu.ro>
2022-09-29 17:52:48 +0300

    reorganize and remove duplicate code

--- Vlad/Task1_scripting/script1.sh
+++ Vlad/Task1_scripting/script1.sh
@@ -1,29 +1,60 @@
 #! /bin/bash
 
-echo "Starting script..."
-# 1-3
-cd ~/Desktop/ausy_academy_2022/sandbox/vvestemeanu
-# 4-5
-git switch -c "Vlad_Script1"
-# 6
-touch "$MY_NAME.txt"
-# 7
-for i in {1..10}
+scriptV1(){
+
+for i in {0..9}
 do
     touch "gitMessage$i"
-    # 8
     if [ $i -eq 5 ]; then
         echo "$MY_NAME: my first script" >> gitMessage5
     fi
+    if [ $i -eq 7 ]; then
+        echo "$BRANCH_NAME" >> gitMessage7
+    fi
 done
-# 9
-git add "$MY_NAME.txt"
-# 10
-commitmessage="$(grep "$MY_NAME: my first script" *)"
-# 11
-git commit -m "$commitmessage"
-# 12
-for i in {1..10}
+for i in {0..9}
+do
+    if [ -s gitMessage$i ]; then
+        commitmessage="${commitmessage}$(cat gitMessage$i);"
+    fi
+done
+for i in {0..9}
 do
     rm "gitMessage$i"
 done
+}
+
+scriptV2(){
+
+touch gitMessage{0..9}
+echo "$MY_NAME: my first script" >> "$(find *5)"
+echo "$BRANCH_NAME" >> "$(find *7)"
+commitmessage="$(grep -l "." gitMessage* | xargs cat)"
+rm gitMessage{0..9}
+}
+
+echo "Starting script..."
+
+BRANCH_NAME="$1"
+cd ~/Desktop/ausy_academy_2022/sandbox/vvestemeanu
+git switch -c "$BRANCH_NAME"
+touch "$MY_NAME.txt"
+
+case "$2" in
+"0")
+    scriptV1
+;;
+"1")
+    scriptV2
+;;
+*)
+    scriptV1
+;;
+esac
+
+git add "$MY_NAME.txt"
+git add ~/Desktop/ausy_academy_2022/Vlad/Task1_scripting/script1.sh
+git commit -m "$commitmessage"
+git push origin $BRANCH_NAME
+
+echo "Done!"
